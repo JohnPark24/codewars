50f352c7252f401b918fdc9c42ef8f695265480f
John Park <shinopark24@gmail.com>
2022-05-28 20:04:57 -0500

    7 kyu NOT complete

---
+++ 7 kyu/Mumbling.js
@@ -0,0 +1,18 @@
+function accum(s) {
+    let newString = '';
+      let chars = s.split();
+    
+    for(let i = 0; i < chars.length; i++){
+      
+    }
+    return newString;
+  }
+  
+  //split the string
+  //separate each letter with '-'
+  //capitalize the first letter of each letter string
+  //add accumitaveldfsafd
+  
+  
+  //string to array back to string
+  //chars.join
