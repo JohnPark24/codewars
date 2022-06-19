7a48e078a6ff907dfe10ee5089eba9cecf640006
John Park <shinopark24@gmail.com>
2022-06-19 16:15:21 -0500

    8 kyu complete

--- 8 kyu/Convert a Number to a String.js
+++ 8 kyu/Convert a Number to a String.js
@@ -1,4 +1,10 @@
-//We need a function that can transform a number into a string.
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
 
 function numberToString(num) {
     return num.toString();
