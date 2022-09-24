664fa6ae65ed878dd4abb16629298dfb4a3f3757
John Park <shinopark24@gmail.com>
2022-09-24 16:57:37 -0500

    8 kyu complete

--- 8 kyu/Grasshopper - Terminal game combat function.js
+++ 8 kyu/Grasshopper - Terminal game combat function.js
@@ -1,6 +1,9 @@
 // Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
 
 function combat(health, damage) {
-    let total = health - damage;
-    return (total) >= 0 ? total : 0;
+    return health < damage ? 0 : health - damage;
+}
+
+function combat(health, damage) {
+    return Math.max(health - damage, 0);
 }
