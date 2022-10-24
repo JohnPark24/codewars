6ed03f1cb16e69a904cc839d7d2fe802f4f30036
John Park <shinopark24@gmail.com>
2022-10-24 16:33:25 -0500

    8 kyu complete

--- 7 kyu/Reverse words.js
+++ 7 kyu/Reverse words.js
@@ -13,3 +13,10 @@ function reverseWords(str) {
 
 
 let reverseWords = (str) => str.replace(/\S+/g, word => [...word].reverse().join(""));
+
+
+function reverseWords(str) {
+    return str.split(' ').map(function(word){
+      return word.split('').reverse().join('');
+    }).join(' ');
+  }
