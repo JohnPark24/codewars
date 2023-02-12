11ccc852b0ef7c3b72ed63ca24bb2996da157ac7
John Park <shinopark24@gmail.com>
2023-02-12 16:15:47 -0600

    8 kyu complete

---
+++ 8 kyu/Sort and Star.js
@@ -0,0 +1,18 @@
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+function twoSort(s) {
+    let sort = s.sort()
+    let val = '';
+    for(let i = 0; i < sort[0].length - 1; i++){
+        val += sort[0][i] + '***';
+    }
+    return val += sort[0].slice(-1);
+}
+
+function twoSort(s) {
+    return s.sort()[0].split('').join('***');
+}
