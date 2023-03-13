cec51268a01101a2c6fa4019be005bcdb1a54d7c
John Park <shinopark24@gmail.com>
2023-03-13 18:19:59 -0500

    5 kyu complete

---
+++ 5 kyu/Regex Password Validation.js
@@ -0,0 +1,13 @@
+// You need to write regex that will validate a password to make sure it meets the following criteria:
+
+// At least six characters long
+// contains a lowercase letter
+// contains an uppercase letter
+// contains a digit
+// only contains alphanumeric characters (note that '_' is not alphanumeric)
+
+
+// assign your RegExp to REGEXP
+const REGEXP = /^(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]+$/;
+
+const REGEXP = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d]{6,}$/;
