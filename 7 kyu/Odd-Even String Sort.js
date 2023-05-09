5f6689f8c157a9247c2a33413ff2c4336d03a3fa
John Park <shinopark24@gmail.com>
2023-05-08 21:22:31 -0500

    leetcode complete

--- 7 kyu/Odd-Even String Sort.js
+++ 7 kyu/Odd-Even String Sort.js
@@ -51,5 +51,5 @@ function sortMyString(S) {
 const sortMyString = s => {
     let even = s.split('').filter((v, i) => i % 2 === 0).join('')
     let odd = s.split('').filter((v, i) => i % 2 !== 0).join('')
-    return even + ' ' + odd
+    return even + ' ' + odd;
 }
