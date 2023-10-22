650df276ae6f1fc9f8750053f44f58f6e0b36fb9
John Park <shinopark24@gmail.com>
2023-10-21 23:32:18 -0500

    codewar complete

---
+++ 7 kyu/Numbers to Letters.js
@@ -0,0 +1,46 @@
+// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+
+function switcher(arr) {
+    const reversedMapping = ' zyxwvutsrqponmlkjihgfedcba!?';
+    let result = '';
+  
+    for (const numStr of arr) {
+      const num = parseInt(numStr, 10);
+      if (!isNaN(num)) {
+        if (num >= 1 && num <= 26) {
+          // Map numbers 1 to 26 to letters.
+          result += reversedMapping[num];
+        } else if (num === 27) {
+          // Handle '!' (mapped to 27).
+          result += '!';
+        } else if (num === 28) {
+          // Handle '?' (mapped to 28).
+          result += '?';
+        } else if (num === 29) {
+          // Handle ' ' (space, mapped to 29).
+          result += ' ';
+        }
+      }
+    }
+    return result;
+}
+
+
+const alpha = ' zyxwvutsrqponmlkjihgfedcba!? '
+const switcher = x => x.map(item => alpha[item]).join('')
+
+
+function switcher(x)
+{
+  return x.map(a => 
+    {
+      if(a == 27) return "!";
+      if(a == 28) return "?";
+      if(a == 29) return " ";
+      return String.fromCharCode(97 + 26 - parseInt(a));
+      
+    }).join('');
+}
