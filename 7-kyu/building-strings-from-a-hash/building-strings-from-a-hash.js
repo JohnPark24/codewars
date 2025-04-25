5540c35469017dc436590a58c0206ba2f26b687a
John Park <shinopark24@gmail.com>
2025-04-24 21:14:09 -0500

    Update solution - CodeHub

--- 7-kyu/building-strings-from-a-hash/building-strings-from-a-hash.js
+++ 7-kyu/building-strings-from-a-hash/building-strings-from-a-hash.js
@@ -1,8 +1,6 @@
+ 
 function solution(pairs){
-  let result = '';
-  
-  for(const [key, value] of Object.entries(pairs)){
-    result += `${key} = ${value},`;
-  }
-  return result.slice(0,-1);
+  return Object.keys(pairs)
+    .map(([key,value]) => `${key} = ${value}`)
+    .join(',');
 }
